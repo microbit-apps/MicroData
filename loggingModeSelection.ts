6a52a35ce68810094703a819ee8c399270c0802c
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-07-03 16:30:27 +0100

    loggingModeSelection.ts: Interval mode rework.

--- loggingModeSelection.ts
+++ loggingModeSelection.ts
@@ -1,6 +1,8 @@
+
 namespace microdata {
     import Screen = user_interface_base.Screen
-    import CursorScene = user_interface_base.CursorScene
+    import Scene = user_interface_base.Scene
+    import CursorSceneWithPriorPage = user_interface_base.CursorSceneWithPriorPage
     import Button = user_interface_base.Button
     import ButtonStyles = user_interface_base.ButtonStyles
     import AppInterface = user_interface_base.AppInterface
@@ -8,15 +10,261 @@ namespace microdata {
     import font = user_interface_base.font
     import Bounds = user_interface_base.Bounds
 
-    export class LoggingModeSelection extends CursorScene {
+    const enum GUI_STATE {
+        CHANGING_NUMBER_OF_MEASUREMENTS,
+        CHANGING_PERIOD_SECONDS
+    }
+
+    export class BasicLoggingScene extends Scene {
+        private guiState: GUI_STATE = GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS;
+        private numberOfMeasurements: number = 10;
+        private periodSeconds: number = 1;
+
         constructor(app: AppInterface) {
             super(app)
         }
 
         /* override */ startup() {
             super.startup()
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.right.id,
+                () => {
+                    this.guiState =
+                    (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS) ?
+                        this.guiState = GUI_STATE.CHANGING_PERIOD_SECONDS :
+                        this.guiState = GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS
+                }
+            )
 
-            const y = 36
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.left.id,
+                () => {
+                    this.guiState =
+                    (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS) ?
+                        this.guiState = GUI_STATE.CHANGING_PERIOD_SECONDS :
+                        this.guiState = GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.up.id,
+                () => {
+                    let tick = true;
+                    control.onEvent(
+                        ControllerButtonEvent.Released,
+                        controller.up.id,
+                        () => tick = false
+                    )
+
+                    // Control logic:
+                    while (tick) {
+                        if (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS)
+                            this.numberOfMeasurements += 1
+                        else
+                            this.periodSeconds += 1
+                        basic.pause(100)
+                    }
+                    // Reset binding
+                    control.onEvent(ControllerButtonEvent.Released, controller.up.id, () => { })
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.down.id,
+                () => {
+                    let tick = true;
+                    control.onEvent(
+                        ControllerButtonEvent.Released,
+                        controller.down.id,
+                        () => tick = false
+                    )
+
+                    // Control logic:
+                    while (tick) {
+                        if (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS)
+                            this.numberOfMeasurements = Math.max(1, this.numberOfMeasurements - 1)
+                        else
+                            this.periodSeconds = Math.max(1, this.periodSeconds - 1)
+                        basic.pause(100)
+                    }
+                    // Reset binding
+                    control.onEvent(ControllerButtonEvent.Released, controller.down.id, () => { })
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.A.id,
+                () => {
+                    
+                    basic.showLeds(`
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        # . . . #
+                        . # # # .
+                    `)
+                }
+            )
+        }
+
+        draw() {
+            screen().fill(12);
+
+            const textFont = bitmaps.font12;
+            const textXOffset = 30;
+            const numberOfMeasurementsCenteringOffset =
+                ((this.numberOfMeasurements.toString().length * textFont.charWidth) >> 1);
+            const periodCenteringOffset =
+                ((this.periodSeconds.toString().length * textFont.charWidth) >> 1);
+
+            const triangleWidth = 22;
+
+            const sensorName = "Accel. X"
+            screen().fillRect(
+                (screen().width >> 1) - ((sensorName.length * font.charWidth) >> 1) + 2,
+                5 + 2,
+                (sensorName.length * font.charWidth) + 6 + 1,
+                font.charHeight + 3 + 1,
+                15
+            )
+
+            screen().fillRect(
+                (screen().width >> 1) - ((sensorName.length * font.charWidth) >> 1),
+                5,
+                (sensorName.length * font.charWidth) + 6,
+                font.charHeight + 3,
+                6
+            )
+
+            screen().fillRect(
+                textXOffset - numberOfMeasurementsCenteringOffset - 3,
+                (screen().height >> 1) - (textFont.charHeight >> 1) + 1,
+                (numberOfMeasurementsCenteringOffset * 2) + 2,
+                textFont.charHeight + 3,
+                15
+            )
+
+            screen().fillRect(
+                textXOffset - numberOfMeasurementsCenteringOffset - 4,
+                (screen().height >> 1) - (textFont.charHeight >> 1),
+                (numberOfMeasurementsCenteringOffset * 2) + 1,
+                textFont.charHeight + 2,
+                4
+            )
+
+            screen().fillRect(
+                screen().width - textXOffset - periodCenteringOffset - 3,
+                (screen().height >> 1) - (textFont.charHeight >> 1) + 1,
+                (periodCenteringOffset * 2) + 2,
+                textFont.charHeight + 3,
+                15
+            )
+
+            screen().fillRect(
+                screen().width - textXOffset - periodCenteringOffset - 4,
+                (screen().height >> 1) - (textFont.charHeight >> 1),
+                (periodCenteringOffset * 2) + 1,
+                textFont.charHeight + 2,
+                4
+            )
+
+            for (let i = 0; i < 2; i++) { // Embolden
+                screen().print(
+                    sensorName,
+                    (screen().width >> 1) - ((sensorName.length * font.charWidth) >> 1) + 4 + i,
+                    5 + 2,
+                    15
+                )
+
+                screen().print(
+                    "" + this.numberOfMeasurements,
+                    textXOffset - i - numberOfMeasurementsCenteringOffset,
+                    (screen().height >> 1) - (textFont.charHeight >> 1),
+                    15,
+                    textFont
+                )
+
+                screen().print(
+                    "" + this.periodSeconds,
+                    screen().width - textXOffset - i - periodCenteringOffset,
+                    (screen().height >> 1) - (textFont.charHeight >> 1),
+                    15,
+                    textFont
+                )
+            }
+
+            let xOffset = 
+                (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS) ?
+                    screen().width - 30: (screen().width >> 1);
+
+            screen().fillRect(
+                xOffset - (screen().width >> 1)  + 2,
+                25 - 1,
+                (screen().width >> 1) + 24,
+                screen().height - 40,
+                15
+            )
+
+            screen().fillRect(
+                xOffset - (screen().width >> 1) + 4,
+                25 + 1,
+                (screen().width >> 1) + 24 - 5,
+                screen().height - 40 - 5,
+                6
+            )
+
+            screen().fillRect(
+                xOffset - (screen().width >> 1) + 6,
+                25 + 6,
+                3,
+                3,
+                15
+            )
+
+            const tips = (this.guiState == GUI_STATE.CHANGING_NUMBER_OF_MEASUREMENTS) ?
+                ["Use Up and Down\nto change the\nnumber of\nmeasurements.",
+                 "Press Right to\nprogress."] :
+                ["Use Up and Down\nto change the\nseconds between\nmeasurements.",
+                 "Press A when\nyou're done."];
+
+            screen().print(
+                tips[0],
+                xOffset - (screen().width >> 1) + 10,
+                25 + 4,
+                15
+            )
+
+            screen().fillRect(
+                xOffset - (screen().width >> 1) + 6,
+                75 + 6,
+                3,
+                3,
+                15
+            )
+
+            screen().print(
+                tips[1],
+                xOffset - (screen().width >> 1) + 10,
+                75 + 4,
+                15
+            )
+        }
+    }
+
+    export class LoggingModeSelection extends CursorSceneWithPriorPage {
+        constructor(app: AppInterface) {
+            super(app, () => {this.app.popScene(); this.app.pushScene(new Home(this.app))})
+        }
+
+        /* override */ startup() {
+            super.startup()
+
+            const y = 36;
 
             this.navigator.setBtns([[
                 new Button({
@@ -28,7 +276,7 @@ namespace microdata {
                     y,
                     onClick: () => {
                         this.app.popScene()
-                        this.app.pushScene(new SensorSelect(this.app, CursorSceneEnum.LiveDataViewer))
+                        this.app.pushScene(new BasicLoggingScene(this.app))
                     },
                 }),
 
@@ -136,8 +384,8 @@ namespace microdata {
 
                 case "largeSettingsGear": {
                     const tips = [
-                        "Log a sudden change in\nacceleration.",
-                        "Or when temperature\nreaches a certain value."
+                        "Log when there's a\nsudden change.",
+                        "Or when a certain value\n is met."
                     ];
                     drawTutorialTips("Event Mode", tips)
                     break;
@@ -157,3 +405,4 @@ namespace microdata {
         }
     }
 }
+
