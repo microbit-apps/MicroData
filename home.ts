553aa522223767a733f78da84243132f2aaf055a
KierPalin <45743174+KierPalin@users.noreply.github.com>
2026-06-05 16:25:56 +0100

    Home screen replication + questions

--- home.ts
+++ home.ts
@@ -1,137 +1,132 @@
 namespace microdata {
   export class Home extends ui.UiScreen {
-    private toggleBtn: ui.UiButton;
-    private status: "Ready" | "Started" | "Stopped";
+    private btns: ui.UiButton[];
 
     constructor(runtime: ui.UiRuntime) {
       super(runtime);
 
-      const y = 25
+      this.btns = [
+        new ui.UiButton({
+          id: "btn1",
+          focusLabel: "Real-time Data",
+          bitmap: linearGraph1,
+          onActivate: () => {
+            this.runtime.pop()
+          },
+          style: ui.UiButtonStyles.Transparent,
+          size: { width: 30, height: 30 }
+        }),
 
-      // this.btn =  new Button({
-      //   parent: null,
-      //   style: ButtonStyles.Transparent,
-      //   icon: "linear_graph_1",
-      //   ariaId: "Real-time Data",
-      //   x: -58,
-      //   y,
-      //   onClick: () => {
-      //     this.app.popScene()
-      //     this.app.pushScene(new SensorSelect(this.app, MicroDataSceneEnum.LiveDataViewer))
-      //   },
-      // }),
+        new ui.UiButton({
+          id: "btn2",
+          focusLabel: "Log Data",
+          bitmap: largeEditIcon,
+          onActivate: () => {
+            this.runtime.pop()
+          },
+          style: ui.UiButtonStyles.Transparent,
+          size: { width: 30, height: 30 }
+        }),
 
-      this.toggleBtn = new ui.UiButton("toggle", "Real-time data", () => {
-        this.status = this.status == "Started" ? "Stopped" : "Started"
-        this.toggleBtn.setText(
-          this.status == "Started" ? "Stop" : "Start",
-        );
-      })
+        new ui.UiButton({
+          id: "btn3",
+          focusLabel: "Command Mode",
+          bitmap: radio_set_group,
+          onActivate: () => {
+            this.runtime.pop()
+          },
+          style: ui.UiButtonStyles.Transparent,
+          size: { width: 30, height: 30 }
+        }),
 
-      // new Button({
-      //   parent: null,
-      //   style: ButtonStyles.Transparent,
-      //   icon: "edit_program",
-      //   ariaId: "Log Data",
-      //   x: -20,
-      //   y,
-      //   onClick: () => {
-      //     this.app.popScene()
-      //     this.app.pushScene(new SensorSelect(this.app, MicroDataSceneEnum.RecordingConfigSelect))
-      //   },
-      // }),
+        new ui.UiButton({
+          id: "btn4",
+          focusLabel: "View Data & Settings",
+          bitmap: largeDisk,
+          onActivate: () => {
+            this.runtime.pop()
+          },
+          style: ui.UiButtonStyles.Transparent,
+          size: { width: 30, height: 30 }
+        })
+      ];
 
-      // new Button({
-      //   parent: null,
-      //   style: ButtonStyles.Transparent,
-      //   icon: "radio_set_group",
-      //   ariaId: "Command Mode",
-      //   x: 20,
-      //   y,
-      //   onClick: () => {
-      //     this.app.popScene()
-      //     this.app.pushScene(new DistributedLoggingScreen(this.app))
-      //   },
-      // }),
-
-      // new Button({
-      //   parent: null,
-      //   style: ButtonStyles.Transparent,
-      //   icon: "largeDisk",
-      //   ariaId: "View Data & Settings",
-      //   x: 58,
-      //   y,
-      //   onClick: () => {
-      //     this.app.popScene()
-      //     this.app.pushScene(new DataViewSelect(this.app))
-      //   },
-      // })
+      const centerY: number = ui.STANDARD_DISPLAY_HEIGHT - 32;
+      this.btns.forEach((btn, idx) => this.add(btn, { centerX: 20 + (40 * idx), centerY }));
     }
 
-    // private drawVersion() {
-    //   const font = bitmaps.font5
-    //   const text = "v1.7.5"
-    //   Screen.print(
-    //     text,
-    //     Screen.RIGHT_EDGE - (font.charWidth * text.length),
-    //     Screen.BOTTOM_EDGE - font.charHeight - 2,
-    //     0xb,
-    //     font
-    //   )
-    // }
+    // What do we think about handling left/right this way?
+    // What about the btn ids "btn1", "btn2", etc? Should these be local names?
+    // It feels very responsive which is great.
+    // How can I reduce the repeat tick speed?
+    //
+    // Text label Z-height is wrong
+    public handleInput(event: ui.UiInputEvent): boolean | undefined {
+      let handled = super.handleInput(event);
+      if (handled !== undefined) return handled;
 
-    // private yOffset = -Screen.HEIGHT >> 1
+      const ids = this.btns.map(b => b.scopeId)
+      const idx = ids.indexOf(this.focus.getActiveScopeId())
+
+      if (idx === -1) return undefined // Maybe we want it to crash?
+
+      if (event.action === "left" && (event.phase === "pressed" || event.phase === "repeated" )) {
+        this.focus.setActiveScope(ids[(idx - 1 + this.btns.length) % this.btns.length]); return true;
+      }
+
+      if (event.action === "right" && (event.phase === "pressed" || event.phase === "repeated")) {
+        this.focus.setActiveScope(ids[(idx + 1) % this.btns.length]); return true;
+      }
+
+      return undefined;
+    }
+
+    private yOffset = -ui.STANDARD_DISPLAY_HEIGHT >> 1
     render(surface: ui.DrawSurface): void {
-      surface.fillRect(new ui.Rect(0, 0, 160, 120), 0x12);
-      surface.drawBitmap(this.assets.getBitmap("microdataLogo"), 0, 0);
+      this.yOffset = Math.min(0, this.yOffset + 2)
+      const t = control.millis()
+      const dy = this.yOffset == 0 ? (Math.idiv(t, 800) & 1) - 1 : 0
+      const margin = 2
+      const OFFSET = (ui.STANDARD_DISPLAY_HEIGHT >> 1) - microdataLogo.height - margin - 9
+      const y = OFFSET
 
-      // const microdataLogo = Icons.get("microdataLogo")
+      surface.clear(0xC);
 
-      // this.btn.render
+      surface.drawBitmap(
+        microdataLogo,
+        ((ui.STANDARD_DISPLAY_WIDTH - microdataLogo.width) >> 1) + dy,
+        y + this.yOffset
+      );
 
-      // this.yOffset = Math.min(0, this.yOffset + 2)
-      // const t = control.millis()
-      // const dy = this.yOffset == 0 ? (Math.idiv(t, 800) & 1) - 1 : 0
-      // const margin = 2
-      // const OFFSET = (Screen.HEIGHT >> 1) - microdataLogo.height - margin - 9
-      // const y = Screen.TOP_EDGE + OFFSET //+ dy
-      // Screen.drawTransparentImage(
-      //   microdataLogo,
-      //   Screen.LEFT_EDGE + ((Screen.WIDTH - microdataLogo.width) >> 1)// + dy
-      //   ,
-      //   y + this.yOffset
-      // )
-      //
-      // Screen.drawTransparentImage(
-      //   microbitLogo,
-      //   Screen.LEFT_EDGE +
-      //   ((Screen.WIDTH - microbitLogo.width) >> 1) + dy
-      //   ,
-      //   y - microdataLogo.height + this.yOffset + margin
-      // )
-      //
-      // if (!this.yOffset) {
-      //   Screen.print(
-      //     "Mini-measurer",
-      //     Screen.LEFT_EDGE +
-      //     ((Screen.WIDTH + microdataLogo.width) >> 1)
-      //     + dy
-      //     -
-      //     font.charWidth * "Mini-measurer".length,
-      //     Screen.TOP_EDGE +
-      //     OFFSET +
-      //     microdataLogo.height +
-      //     dy +
-      //     this.yOffset +
-      //     3,
-      //     0xb,
-      //     font
-      //   )
-      // }
-      //
-      // this.navigator.drawComponents();
-      // this.drawVersion()
-      // super.draw()
+      surface.drawBitmap(
+        microbitLogo,
+        ((ui.STANDARD_DISPLAY_WIDTH - microbitLogo.width) >> 1) + dy,
+        y - microdataLogo.height + this.yOffset + margin
+      );
+
+      if (!this.yOffset) {
+        const size: ui.Size = surface.measureText("direct", bitmaps.font8)
+        surface.drawText(
+          "Mini-measurer",
+          ((ui.STANDARD_DISPLAY_WIDTH + microdataLogo.width) >> 1) + dy - (size.width << 1),
+          OFFSET + microdataLogo.height + dy + this.yOffset + 3,
+          { color: 1, font: bitmaps.font8 }
+        )
+      }
+
+      // draw version:
+      const font = bitmaps.font5
+      const text = "v1.9.0"
+      const size: ui.Size = surface.measureText("direct", font)
+
+      surface.drawText(
+        text,
+        ui.STANDARD_DISPLAY_WIDTH - size.width,
+        ui.STANDARD_DISPLAY_HEIGHT - size.height - 2,
+        { color: 0xb, font }
+      )
+
+      super.render(surface) // btns
     }
   }
 }
